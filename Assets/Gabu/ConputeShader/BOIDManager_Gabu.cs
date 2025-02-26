bae2c52d759bfaf3bb576fec13e6e685f10a8a0a
GabuGabuNoMi <139324502+IsooGabriel@users.noreply.github.com>
2025-02-26 14:32:04 +0900

    タイトルのBOID完成

--- Assets/Gabu/ConputeShader/BOIDManager_Gabu.cs
+++ Assets/Gabu/ConputeShader/BOIDManager_Gabu.cs
@@ -3,7 +3,6 @@ using System.Runtime.InteropServices;
 using Unity.Collections;
 using Unity.Mathematics;
 using UnityEngine;
-using UnityEngine.Rendering;
 using UnityEngine.VFX;
 using Random = Unity.Mathematics.Random;
 
@@ -23,29 +22,38 @@ public class BOIDManager_Gabu : MonoBehaviour
         public float moveSpeed = 1f;
 
         [Range(0f, 1f)] public float separationWeight = .5f;
-
         [Range(0f, 1f)] public float alignmentWeight = .5f;
-
         [Range(0f, 1f)] public float targetWeight = .5f;
 
         public Transform boidTarget;
     }
 
-    public int boidCount = 32;
+    public int startCount = 1;
+    public int endCount = 100;
+    private int boidCount;
+    public float instanceTime = 60f; // 60秒ごとにBOID数を更新
 
     public float3 boidExtent = new(32f, 32f, 32f);
-
     public ComputeShader BoidComputeShader;
-
     public BoidConfig boidConfig;
 
-    VisualEffect _boidVisualEffect;
-    GraphicsBuffer _boidBuffer;
-    int _kernelIndex;
+    private VisualEffect _boidVisualEffect;
+    private GraphicsBuffer _boidBuffer;
+    private int _kernelIndex;
+    private float _timer;
 
-    void OnEnable()
+    private void Awake()
     {
-        _boidBuffer = PopulateBoids(boidCount, boidExtent);
+        _timer = 0;
+        boidCount = startCount;
+        InitializeBoids();
+    }
+
+    private void InitializeBoids()
+    {
+        _boidBuffer?.Dispose();
+        _boidBuffer = PopulateBoids(boidCount, boidExtent, null); // 既存データなしで初期化
+
         _kernelIndex = BoidComputeShader.FindKernel("CSMain");
         BoidComputeShader.SetBuffer(_kernelIndex, "boidBuffer", _boidBuffer);
         BoidComputeShader.SetInt("numBoids", boidCount);
@@ -54,37 +62,84 @@ public class BOIDManager_Gabu : MonoBehaviour
         _boidVisualEffect.SetGraphicsBuffer("Boids", _boidBuffer);
     }
 
-    void OnDisable()
-    {
-        _boidBuffer?.Dispose();
-    }
+    void OnEnable() => InitializeBoids();
+
+    void OnDisable() => _boidBuffer?.Dispose();
 
     void Update()
     {
+        _timer += Time.deltaTime;
+        if (_timer >= instanceTime && boidCount < endCount)
+        {
+            _timer = 0;
+            int newBoidCount = Mathf.Min(boidCount + 10, endCount); // 10体ずつ増やす
+            UpdateBoidsCount(newBoidCount);
+        }
         UpdateBoids();
     }
 
     void UpdateBoids()
     {
-        var boidTarget = boidConfig.boidTarget != null
-            ? boidConfig.boidTarget.position
-            : transform.position;
+        var boidTarget = boidConfig.boidTarget ? boidConfig.boidTarget.position : transform.position;
         BoidComputeShader.SetFloat("deltaTime", Time.deltaTime);
         BoidComputeShader.SetFloat("separationWeight", boidConfig.separationWeight);
         BoidComputeShader.SetFloat("alignmentWeight", boidConfig.alignmentWeight);
         BoidComputeShader.SetFloat("targetWeight", boidConfig.targetWeight);
         BoidComputeShader.SetFloat("moveSpeed", boidConfig.moveSpeed);
         BoidComputeShader.SetVector("targetPosition", boidTarget);
-        BoidComputeShader.GetKernelThreadGroupSizes(_kernelIndex, out var x, out var y, out var z);
-        BoidComputeShader.Dispatch(_kernelIndex, (int)(boidCount / x), 1, 1);
+
+        BoidComputeShader.GetKernelThreadGroupSizes(_kernelIndex, out var x, out _, out _);
+        BoidComputeShader.Dispatch(_kernelIndex, math.max(1, boidCount / (int)x), 1, 1);
     }
 
-    public static GraphicsBuffer PopulateBoids(int boidCount, float3 boidExtent)
+    /// <summary>
+    /// BOID の数を増やす (位置をリセットせず、新しい BOID だけ追加)
+    /// </summary>
+    void UpdateBoidsCount(int newBoidCount)
+    {
+        if (newBoidCount <= boidCount) return;
+
+        // 現在の BOID データを取得
+        var oldBoidArray = new NativeArray<BoidState>(boidCount, Allocator.Temp);
+        BoidState[] tempArray = new BoidState[boidCount];
+        _boidBuffer.GetData(tempArray);
+        NativeArray<BoidState>.Copy(tempArray, oldBoidArray);
+
+
+        // 新しい BOID を追加
+        _boidBuffer?.Dispose();
+        _boidBuffer = PopulateBoids(newBoidCount, boidExtent, oldBoidArray);
+        oldBoidArray.Dispose();
+
+        BoidComputeShader.SetBuffer(_kernelIndex, "boidBuffer", _boidBuffer);
+        BoidComputeShader.SetInt("numBoids", newBoidCount);
+        _boidVisualEffect.SetGraphicsBuffer("Boids", _boidBuffer);
+
+        boidCount = newBoidCount;
+    }
+
+    /// <summary>
+    /// BOID を生成する。`oldBoids` が指定されていれば、既存データを維持しながら新しい BOID を追加
+    /// </summary>
+    public static GraphicsBuffer PopulateBoids(int boidCount, float3 boidExtent, NativeArray<BoidState>? oldBoids)
     {
         var random = new Random(256);
-        var boidArray =
-            new NativeArray<BoidState>(boidCount, Allocator.Temp, NativeArrayOptions.UninitializedMemory);
-        for (var i = 0; i < boidArray.Length; i++)
+        var boidArray = new NativeArray<BoidState>(boidCount, Allocator.Temp, NativeArrayOptions.UninitializedMemory);
+
+        int oldBoidCount = oldBoids?.Length ?? 0;
+
+        // 既存 BOID をコピー
+        if (oldBoids.HasValue)
+        {
+            NativeArray<BoidState> oldBoidArray = oldBoids.Value;
+            for (int i = 0; i < oldBoidCount; i++)
+            {
+                boidArray[i] = oldBoidArray[i];
+            }
+        }
+
+        // 新しい BOID を追加
+        for (int i = oldBoidCount; i < boidArray.Length; i++)
         {
             boidArray[i] = new BoidState
             {
@@ -92,8 +147,8 @@ public class BOIDManager_Gabu : MonoBehaviour
                 Forward = math.rotate(random.NextQuaternionRotation(), Vector3.forward),
             };
         }
-        var boidBuffer =
-            new GraphicsBuffer(GraphicsBuffer.Target.Structured, boidArray.Length, Marshal.SizeOf<BoidState>());
+
+        var boidBuffer = new GraphicsBuffer(GraphicsBuffer.Target.Structured, boidArray.Length, Marshal.SizeOf<BoidState>());
         boidBuffer.SetData(boidArray);
         boidArray.Dispose();
         return boidBuffer;
