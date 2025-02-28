b1b6a6207a343dfa309c978650d22477346e0551
suzuten7 <suzukit0112@gmail.com>
2025-02-28 14:28:29 +0900

    39

--- Assets/00MyAssets/Script/Player/Player_Target.cs
+++ Assets/00MyAssets/Script/Player/Player_Target.cs
@@ -8,8 +8,8 @@ public class Player_Target : MonoBehaviour
     [SerializeField] Camera Cam;
     [SerializeField] int TargetTimes;
     [SerializeField] GameObject TargetObj;
-    int StayTime;
-
+    int StayTime = 0;
+    int AutoTime = 0;
     void FixedUpdate()
     {
         if (PCont.Target_Stay)
@@ -18,21 +18,7 @@ public class Player_Target : MonoBehaviour
             if (StayTime >= TargetTimes)
             {
                 Sta.TargetHit = null;
-                float NearDis = -1;
-                foreach (var Hits in BTManager.HitList)
-                {
-                    if (!TeamCheck(Sta, Hits.Sta)) continue;
-                    var CPos = Cam.WorldToViewportPoint(Hits.PosGet());
-                    if (CPos.z <= 0) continue;
-                    CPos.x -= 0.5f;
-                    CPos.y -= 0.5f;
-                    float Dis = new Vector2(CPos.x, CPos.y).magnitude;
-                    if (NearDis < 0 || NearDis > Dis)
-                    {
-                        NearDis = Dis;
-                        Sta.TargetHit = Hits;
-                    }
-                }
+                TargetSet();
             }
         }
         else if (StayTime > 0)
@@ -43,7 +29,37 @@ public class Player_Target : MonoBehaviour
             }
             StayTime = 0;
         }
-        if(Sta.TargetHit!=null && Sta.TargetHit.Sta.HP<=0)Sta.TargetHit = null;
+        AutoTime--;
+        if (Sta.TargetHit != null && Sta.TargetHit.Sta.HP <= 0)
+        {
+            AutoTime = 30;
+        }
+        if (AutoTime > 0)
+        {
+            Sta.TargetHit = null;
+            TargetSet();
+            if (Sta.TargetHit != null) AutoTime = 0;
+        }
+    }
+    void TargetSet()
+    {
+        float NearDis = -1;
+        foreach (var Hits in BTManager.HitList)
+        {
+            if (!TeamCheck(Sta, Hits.Sta)) continue;
+            var CPos = Cam.WorldToViewportPoint(Hits.PosGet());
+            if (CPos.z <= 0) continue;
+            if (CPos.x < 0f || CPos.x > 1f) continue;
+            if (CPos.y < 0f || CPos.y > 1f) continue;
+            CPos.x -= 0.5f;
+            CPos.y -= 0.5f;
+            float Dis = new Vector2(CPos.x, CPos.y).magnitude;
+            if (NearDis < 0 || NearDis > Dis)
+            {
+                NearDis = Dis;
+                Sta.TargetHit = Hits;
+            }
+        }
     }
     private void LateUpdate()
     {
