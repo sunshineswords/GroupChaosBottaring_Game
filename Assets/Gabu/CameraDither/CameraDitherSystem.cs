090cb6e5beaa8e921ee6f546bc3048b15a8a21c1
GabuGabuNoMi <139324502+IsooGabriel@users.noreply.github.com>
2025-04-16 15:01:54 +0900

    木の修正

---
+++ Assets/Gabu/CameraDither/CameraDitherSystem.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class CameraDitherSystem : MonoBehaviour
+{
+    [SerializeField]
+    private Material _material;
+    [SerializeField]
+    private Transform _target;
+    [SerializeField]
+    private string _valueName = "_TargetPosition";
+
+    private void Start()
+    {
+        if (_material == null)
+        {
+            Debug.LogError("Material is not assigned.");
+            return;
+        }
+        if (_target == null)
+        {
+            _target = transform;
+        }
+    }
+
+    void LateUpdate()
+    {
+        _material.SetVector(_valueName, _target.position);
+    }
+}
